ca6c95dfee404a7c74e14180bb530ea47ea34ab4
= <=>
2019-11-19 14:35:44 +0530

    build for global search

---
+++ packages/globalsearch/src/component/globalSearchComponent.svelte
@@ -0,0 +1,4 @@
+<script>
+  export let globalSearchStore;
+</script>
+Search Across Grid: <input  bind:value={globalSearchStore.defaultGlobalSearchString}/>
--- packages/globalsearch/src/wrapper/globalsearch.js
+++ packages/globalsearch/src/wrapper/globalsearch.js
@@ -1,7 +1,18 @@
-'use strict';
+import { globalSearchStore } from '../store/gsstore';
+import  GlobalSearchComponent  from '../component/globalSearchComponent.svelte';
 
-module.exports = globalsearch;
+export default class globalSearchWrapper {
+	constructor(config) {
+		this.state = {};
+		this.state.unSubscribe = globalSearchStore.subscribe(value => {
+			this.state.globalSearchStore = value;
+		});
 
-function globalsearch() {
-    // TODO
+	}
+	render() {
+		let GSObj = new GlobalSearchComponent({
+			globalSearchStore,
+			target: document.body
+		});
+	}
 }
